682288a52abdfffb2c694a26b5ee7141dc2601bb
Ørvur Guttesen <og@realview.dk>
2018-04-02 00:16:15 +0200

    small changes

--- website.py
+++ website.py
@@ -75,6 +75,7 @@ class RegistrationForm(Form):
 		username = StringField('Username', [validators.Length(min=4, max=25)])
 		email = StringField('Email Address', [validators.Length(min=6, max=35)])
 		password = PasswordField('New Password', [
+				validators.Length(min=12, max=100),
 				validators.DataRequired(),
 				validators.EqualTo('confirm', message='Passwords must match')
 		])
